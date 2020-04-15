d8ac282763bf11f9540edc9acedbad6e5828432a
John Paxton <pax@speedingplanet.com>
2020-04-15 11:39:00 -0400

    TextInput testing

--- src/common/TextInput.test.tsx
+++ src/common/TextInput.test.tsx
@@ -1,6 +1,6 @@
 // Test files can be:
 // Foo.test.tsx
-// Foo.spec.tsx 
+// Foo.spec.tsx
 // __tests__/Foo.tsx
 
 // Enzyme
@@ -8,7 +8,7 @@
 // https://testing-library.com/docs/react-testing-library
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TextInput from './TextInput';
 
@@ -19,15 +19,32 @@ test('Mathmatical operators', () => {
 });
 
 it('should test addition', () => {
- expect(2 + 3).toEqual(5);
+  expect(2 + 3).toEqual(5);
 });
 
 test('TextInput', () => {
   const labelText = 'Some label';
-  const {getByLabelText} = render(<TextInput label={labelText} id="foo"/>);
+  const { getByLabelText } = render(<TextInput label={labelText} id="foo" />);
   // const element = getByLabelText(/some label/i);
   // const element = getByLabelText(new RegExp('some label', 'i'));
   const element = getByLabelText(labelText);
 
   expect(element).toBeInTheDocument();
-})
+});
+
+test('TextInput updateField event handler runs', () => {
+  const label = 'Test label';
+  const id = 'Test id';
+  // const handler = jest.fn(() => console.log('Do something'));
+  const handler = jest.fn();
+
+  expect(handler).not.toHaveBeenCalled();
+
+  const { getByLabelText } = render(
+    <TextInput label={label} id={id} updateField={handler} />,
+  );
+  const element = getByLabelText(label);
+  fireEvent.change(element, { target: { value: 'foo' } });
+
+  expect(handler).toHaveBeenCalled();
+});
